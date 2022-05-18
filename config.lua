d3171399a69999edbb70bb6493fde26c3df3960a
wiizz <66723882+wiizzdev@users.noreply.github.com>
2022-05-18 20:00:50 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,125 @@
+Config = {
+    Color = "~p~",
+    Cocktails = {
+        List = {
+            {label = "Punch Exotique", name = 'punchexotique'},
+            {label = "Aperol Spritz", name = 'aperolspritz'},
+            {label = "Piña Colada", name = 'pinacolada'},
+            {label = "Soupe Angevine", name = 'soupeangevine'},
+            {label = "Bière", name = 'margarita'},
+            {label = "Blue Hawaii", name = 'bluehawaii'},
+        },
+        Pos = {
+            {x = -1388.34, y = -612.82, z = 30.32},
+            {x = -1391.91, y = -608.39, z = 30.32},
+        },
+    },
+    Coffre = {
+        Pos = {
+            {x = -1365.81, y = -616.81, z = 30.32},
+        },
+    },
+    Garage = {
+        Vehicle = {
+            {label = "Minibus", name = 'journey'},
+        },
+        Spawn = {p = vector3(-1391.37, -634.11, 28.7), h = 119.06},
+        Pos = {
+            {x = -1397.17, y = -628.88, z = 30.32},
+        },
+        Delete = {
+            {x = -1409.39, y = -635.95, z = 28.67}
+        },
+    },
+    Patron = {
+        Pos = {
+            {x = -1365.81, y = -624.24, z = 30.33}
+        },
+    },
+    Vestiaire = {
+        Pos = {
+            {x = -1367.73, y = -613.28, z = 30.32}
+        },
+        Tenue = {
+            Videur = {
+                {male = {
+                    tshirt_1 = 2,   tshirt_2 = 0,
+                    torso_1 = 22,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 89,   pants_2 = 22,
+                    shoes_1 = 62,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 0,
+                },
+                female = {
+                    tshirt_1 = 14,  tshirt_2 = 0,
+                    torso_1 = 73,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 92,   pants_2 = 22,
+                    shoes_1 = 65,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 1,
+                }},
+            },
+            Barman = {
+                {male = {
+                    tshirt_1 = 2,   tshirt_2 = 0,
+                    torso_1 = 22,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 89,   pants_2 = 22,
+                    shoes_1 = 62,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 0,
+                },
+                female = {
+                    tshirt_1 = 14,  tshirt_2 = 0,
+                    torso_1 = 73,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 92,   pants_2 = 22,
+                    shoes_1 = 65,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 1,
+                }},
+            },
+            Patron = {
+                {male = {
+                    tshirt_1 = 2,   tshirt_2 = 0,
+                    torso_1 = 22,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 89,   pants_2 = 22,
+                    shoes_1 = 62,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 0,
+                },
+                female = {
+                    tshirt_1 = 14,  tshirt_2 = 0,
+                    torso_1 = 73,   torso_2 = 0,
+                    decals_1 = 0,   decals_2 = 0,
+                    chain_1 = 0,    chain_2 = 0,
+                    arms = 0,
+                    pants_1 = 92,   pants_2 = 22,
+                    shoes_1 = 65,   shoes_2 = 6,
+                    helmet_1 = 7,   helmet_2 = 1,
+                }},
+            },
+        },
+    },
+    Markers = {
+        Type = 6,
+        Color = {r = 0, g = 10, b = 113, a = 100},
+    },
+    Blips = {
+        Pos = {
+            {x = -1386.96, y = -589.95, z = 30.0},
+        },
+        Sprite = 93,
+        Scale = 0.6,
+        Colour = 27,
+        Name = "Bahamas",
+    },
+}
